d0f26eeb9648800d167296b7f0ab9c9e25958953
Serifetpck <111680673+Serifetpck@users.noreply.github.com>
2023-06-11 18:20:55 +0300

    Update TestRunner2.java

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber2.xml" },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@web", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
+        tags = "@gp6", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
         dryRun = false
 
 )
